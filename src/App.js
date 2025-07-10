631c64172cc35f2f1e319a4346e9fc10e29ff0dd
purushotham0113 <229x1a33b3@gmail.com>
2025-07-11 00:33:55 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import './App.css';
-import React, { Component } from 'react'
-import NavBar from './components/NavBar';
-import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import About from './components/About';
-import Entertinement from './components/Entertinement';
+import React from 'react'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/about" element={<About />}></Route>
-            <Route path='/business' element={<News key='a' catagory='business' pagesize={6} />}></Route>
-            <Route path='/health' element={<News key='b' catagory='health' pagesize={6} />}></Route>
-            <Route path='/sports' element={<News key='c' catagory='sports' pagesize={6} />}></Route>
-            <Route path='/' element={<News key='d' catagory='general' pagesize={6} />}></Route>
-            <Route path='/technology' element={<News key='e' catagory='technology' pagesize={6} />}></Route>
-            <Route path='/science' element={<News key='f' catagory='science' pagesize={6} />}></Route>
-            <Route path='/entertainment' element={<News key='g' catagory='entertainment' pagesize={6} />}></Route>
-
-          </Routes>
-        </Router>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      <h2>hello world</h2>
+    </div>
+  )
 }
-

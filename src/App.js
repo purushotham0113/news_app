ce5cb6b0cb59bfd11df19c68ea8cefe4029847ee
purushotham0113 <229x1a33b3@gmail.com>
2025-07-12 07:31:34 +0530

    changed app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React from 'react'
-export default function App() {
-  return (
-    <>
-      <h2>this is head 2</h2>
-    </>
-  )
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import News from './components/News';
+import About from './components/About'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<News key="general" catagory="general" pagesize={6} />}
+          />
+          <Route
+            path="/business"
+            element={<News key="business" catagory="business" pagesize={6} />}
+          />
+          <Route
+            path="/entertainment"
+            element={<News key="entertainment" catagory="entertainment" pagesize={6} />}
+          />
+          <Route
+            path="/health"
+            element={<News key="health" catagory="health" pagesize={6} />}
+          />
+          <Route
+            path="/science"
+            element={<News key="science" catagory="science" pagesize={6} />}
+          />
+          <Route
+            path="/sports"
+            element={<News key="sports" catagory="sports" pagesize={6} />}
+          />
+          <Route
+            path="/technology"
+            element={<News key="technology" catagory="technology" pagesize={6} />}
+          />
+          {/* Optional About route if you have an About component */}
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
